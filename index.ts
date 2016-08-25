33e1ba65c1bda1257f05ca4decd5856a9ed9b11d
Andy Knotts <arknotts@gmail.com>
2016-08-24 22:05:19 -0400

    Structure and module refactor.
    Lets us have a cleaner import syntax for consuming code.

---
+++ index.ts
@@ -0,0 +1,8 @@
+import * as Thermostat from './src/thermostat';
+import * as Configuration from './src/configuration';
+import * as TempReader from './src/tempReader';
+import * as TempSensor from './src/tempSensor';
+import * as ThermostatEvent from './src/thermostatEvent';
+import * as Trigger from './src/trigger';
+
+export { Thermostat, Configuration, TempReader, TempSensor, ThermostatEvent, Trigger };
